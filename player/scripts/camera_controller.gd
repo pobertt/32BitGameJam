7137add77da298c79ce409f4f0e1d8adfb8278b7
pobertt <79865756+pobertt@users.noreply.github.com>
2024-07-06 13:38:13 +0100

    fov changes

--- player/scripts/camera_controller.gd
+++ player/scripts/camera_controller.gd
@@ -18,6 +18,8 @@ var pitch_acceleration : float = 15
 var pitch_max : float = 75
 var pitch_min : float = -55
 
+var tween : Tween
+
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
@@ -33,3 +35,10 @@ func _physics_process(delta):
 	pitch_node.rotation_degrees.x = lerp(pitch_node.rotation_degrees.x, pitch, pitch_acceleration * delta)
 	
 	set_cam_rotation.emit(yaw_node.rotation.y)
+	
+func _on_set_movement_state(_movement_state : MovementState):
+	if tween:
+		tween.kill()
+		
+	tween = create_tween()
+	tween.tween_property(camera, "fov", _movement_state.camera_fov, 0.5).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
