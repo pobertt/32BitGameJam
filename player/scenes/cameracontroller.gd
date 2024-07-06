0e2f809a42a3e4c409fdba6a0c49e242878c2e24
pobertt <79865756+pobertt@users.noreply.github.com>
2024-07-06 12:46:04 +0100

    doing player controller

---
+++ player/scenes/cameracontroller.gd
@@ -0,0 +1,23 @@
+extends Node3D
+
+@onready var yaw_node = $camYaw
+@onready var pitch_node = $camYaw/camPitch
+@onready var camera = $camYaw/camPitch/Camera3D
+
+var yaw : float = 0
+var pitch : float = 0
+
+var yaw_sensitivity : float = 0.07
+var pitch_sensitivity : float = 0.07
+
+var yaw_acceleration : float = 15
+var pitch_acceleration : float = 15
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		yaw += -event.relative.x * yaw_sensitivity
+		pitch += event.relative.y * pitch_sensitivity
+
+func _physics_process(delta):
+	yaw_node.rotation_degrees.y = lerp(yaw_node.rotation_degrees.y, yaw, yaw_acceleration * delta)
+	pitch_node.rotation_degrees.x = lerp(pitch_node.rotation_degrees.x, pitch, pitch_acceleration * delta)
