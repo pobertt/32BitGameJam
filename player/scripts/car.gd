9f5284f3c5337fddda2b48399f5f657e1e9a80c3
pobertt <79865756+pobertt@users.noreply.github.com>
2024-07-19 21:12:11 +0100

    multiplayer done and fixed

--- player/scripts/car.gd
+++ player/scripts/car.gd
@@ -14,24 +14,40 @@ var look_at
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$MultiplayerSynchronizer.set_multiplayer_authority(str(name).to_int())
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	look_at = global_position
-
+	if $MultiplayerSynchronizer.is_multiplayer_authority():
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		look_at = global_position
+		
+		$SubViewportContainer.show()
+	else:
+		$SubViewportContainer.hide()
+		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	eDelta = delta
-	if $MultiplayerSynchronizer.get_multiplayer_authority() == multiplayer.get_unique_id():
-		steering = move_toward(steering, Input.get_axis("right", "left") * MAX_STEER, delta * 2.5)
-		engine_force = Input.get_axis("down", "up") * ENGINE_POWER
-		camera_pivot.global_position = camera_pivot.global_position.lerp(global_position, delta * 20.0)
-		camera_pivot.transform = camera_pivot.transform.interpolate_with(transform, delta * 5.0)
-		look_at = look_at.lerp(global_position + linear_velocity, delta * 5.0)
-		camera_3d.look_at(look_at)
-		reverse_camera.look_at(look_at)
-		_check_camera_switch()
-		
+	if $MultiplayerSynchronizer.is_multiplayer_authority():
+		eDelta = delta
+		if $MultiplayerSynchronizer.get_multiplayer_authority() == multiplayer.get_unique_id():
+			steering = move_toward(steering, Input.get_axis("right", "left") * MAX_STEER, delta * 2.5)
+			engine_force = Input.get_axis("down", "up") * ENGINE_POWER
+			camera_pivot.global_position = camera_pivot.global_position.lerp(global_position, delta * 20.0)
+			camera_pivot.transform = camera_pivot.transform.interpolate_with(transform, delta * 5.0)
+			look_at = look_at.lerp(global_position + linear_velocity, delta * 5.0)
+			camera_3d.look_at(look_at)
+			reverse_camera.look_at(look_at)
+			_check_camera_switch()
+			
+			sync_state.rpc(global_position, global_rotation, linear_velocity)
+	else:
+		global_position = global_position.lerp(global_position, delta * 20.0)
+			
 func _check_camera_switch():
 	if linear_velocity.dot(transform.basis.z) > -1:
 		camera_3d.current = true
 	else:
 		reverse_camera.current = true
+		
+@rpc("unreliable")
+func sync_state(pos: Vector3, rot: Vector3, vel: Vector3):
+	global_position = pos
+	global_rotation = rot
+	linear_velocity = vel
--- world/scenes/scene_manager.gd
+++ world/scenes/scene_manager.gd
@@ -9,6 +9,9 @@ func _ready():
 		var currentPlayer = player_scene.instantiate()
 		currentPlayer.name = str(GameManager.players[i].id)
 		add_child(currentPlayer)
+		
+		currentPlayer.set_multiplayer_authority(GameManager.players[i].id)
+		
 		for spawn in get_tree().get_nodes_in_group("PlayerSpawnPoint"):
 			if spawn.name == str(index):
 				currentPlayer.global_position = spawn.global_position
