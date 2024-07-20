605858e60b0bf157b29bed2e978e8e506dc49c44
pobertt <79865756+pobertt@users.noreply.github.com>
2024-07-20 12:03:25 +0100

    added basic main menu

---
+++ world/scripts/settings.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+
+func _on_option_1_pressed():
+	pass # Replace with function body.
+
+
+func _on_option_2_pressed():
+	pass # Replace with function body.
+
+
+func _on_back_pressed():
+	get_tree().change_scene_to_file("res://world/scenes/main_menu.tscn")
